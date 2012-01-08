c438da74446ee4de01e7fc7fa282c907d3d1f13a
David Fox <dsf@seereason.com>
2012-01-08 14:09:45 +0000

    Update Setup.hs to run test script after build

--- Setup.hs
+++ Setup.hs
@@ -1,6 +1,16 @@
 #!/usr/bin/runhaskell
 
 import Distribution.Simple
+import Distribution.Simple.LocalBuildInfo (LocalBuildInfo(buildDir))
 import Distribution.Simple.Program
+import System.Cmd
+import System.Exit
 
-main = defaultMainWithHooks simpleUserHooks
+main = defaultMainWithHooks simpleUserHooks {
+         postBuild = \ _ _ _ lbi -> runTestScript lbi
+       , runTests = \ _ _ _ lbi -> runTestScript lbi
+       }
+
+runTestScript lbi =
+    system (buildDir lbi ++ "/tests/tests") >>= \ code ->
+    if code == ExitSuccess then return () else error "Test Failure"

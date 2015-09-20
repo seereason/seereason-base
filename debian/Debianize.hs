f3d2127d7aa2189a67baada8a1c701f585c0e42b
David Fox <dsf@seereason.com>
2015-09-20 05:02:05 -0700

    Fixes for debianization script

--- debian/Debianize.hs
+++ debian/Debianize.hs
@@ -1,3 +1,4 @@
+{-# LANGUAGE OverloadedStrings #-}
 import Control.Lens
 import Data.Map as Map
 import Debian.AutoBuilder.Details.Versions (seereasonDefaults)
@@ -8,7 +9,7 @@ main :: IO ()
 main = performDebianization $ do
          seereasonDefaults
          (debInfo . sourceFormat) .= Native3
-         (debInfo . control . homepage) ~= Just "http://hackage.haskell.org/package/seereason-base"
+         (debInfo . control . homepage) .= Just "http://hackage.haskell.org/package/seereason-base"
          (debInfo . executable) %= Map.insert (BinPkgName "geni-client")
                                               (InstallFile {execName = "geni-client",
                                                             sourceDir = Nothing, destDir = Nothing,
